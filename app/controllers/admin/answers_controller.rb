84945faaa518abb3b9a687a88d057a33f6bc553d
Laura Nawrocki <lwn2@aol.com>
2015-08-05 21:16:14 -0400

    adding forms, etc.

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -4,4 +4,47 @@ class Admin::AnswersController < ApplicationController
     @answers = Answer.all
   end
 
+  #new answers
+  def new
+      @answer = Answer.find(params[:answer_id])
+      @answer = Answer.new
+    end
+
+    #create
+    def create
+      @answer = Answer.find(params[:answer_id])
+      @answer = Answer.create!(answer_params.merge(question: @question))
+      redirect_to question_answer_path(@question, @answer)
+    end
+
+    #show
+    def show
+      @answer = Answer.find(params[:id])
+    end
+
+    #edit
+    def edit
+      @answer = Answer.find(params[:id])
+    end
+
+    #update
+    def update
+      @answer = Answer.find(params[:id])
+      @question = Question.find(params[:question_id])
+      @answer.update(answer_params.merge(question: @question))
+      redirect_to question_answer_path(@answer.question, @answer)
+    end
+
+    #destroy
+    def destroy
+      @answer = answer.find(params[:id])
+      @answer.destroy
+      redirect_to answers_path
+    end
+
+    private
+    def answer_params
+      params.require(:answer).permit(:content)
+    end
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,44 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+#new Question
+  def new
+    @question = Question.new
+  end
 
+  #create
+  def create
+    @question Question.create!(question_params)
+    redirect_to (question_path(@question))
+  end
+
+  #show
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  #edit
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  #update
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+    redirect_to question_path(@question)
+  end
+
+  #destroy
+  def destroy
+    @question = question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(content)
+  end
+  
 end
